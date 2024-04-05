8bf714af37147ae6e37841c977ed8ef348045064
Willian Oliveira <williancontato67@gmail.com>
2024-04-04 21:50:25 -0300

    Ajustes no responsivo da aplicação

--- src/components/Projetos/ProjetosConteiner/CardEmBreve/CardEmBreve.jsx
+++ src/components/Projetos/ProjetosConteiner/CardEmBreve/CardEmBreve.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SubTitulo from '../../../Home/Titulos/SubTitulo/SubTitulo';
-import { IoSettingsSharp } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
 import ButtonLink from '../../../Home/ButtonLink/ButtonLink'
 import style from './style.module.scss'
 
@@ -9,14 +9,14 @@ const CardEmBreve = () => {
   return (
     <div className={style.conteiner}>
       <div>
-        <SubTitulo>Projetos em breve</SubTitulo>
+        <SubTitulo>Em breve</SubTitulo>
         <p>Em breve, grandes projetos estão a caminho!🚀</p>
         <ButtonLink caminho="/" NavLink={true}>
           Voltar para home
         </ButtonLink>
       </div>
       <div className={style.icon}>
-        <IoSettingsSharp />
+        <IoSettingsOutline />
       </div>
     </div>
   );
